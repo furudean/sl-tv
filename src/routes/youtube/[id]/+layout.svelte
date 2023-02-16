84ca9e5e9061506a7b17a993ca84e32b86fef5ec
Merilynn Bandy <31383109+furudean@users.noreply.github.com>
2023-02-16 01:25:23 -0700

    nit

--- src/routes/youtube/[id]/+layout.svelte
+++ src/routes/youtube/[id]/+layout.svelte
@@ -4,6 +4,7 @@
 	<h1>sl-tv</h1>
 	<p>this is an open source video player, intended to be embedded as media in second life.</p>
 	<p>
-		if you're seeing this in world, you have not cropped the video player very well! go to <a href="/">https://tv.himawari.fun/</a> for help!
+		if you're seeing this in-world, you have not cropped the video player right! go to
+		<a href="/">https://tv.himawari.fun/</a> for help!
 	</p>
 </article>
